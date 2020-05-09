b9cbd7f655b16785960cf56f196f98e9e77fc97b
Wesley Feitosa <jwesleydasilva@gmail.com>
2020-05-09 12:00:56 -0300

    change

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const Details = () => <h1>Details</h1>;
+const Details = ({ match }) => {
+  const { id } = match.params;
+
+  useEffect(() => {
+    console.log(id);
+  }, []);
+
+  return (
+    <>
+      <h1>Details</h1>
+    </>
+  );
+};
 
 export default Details;
+
+Details.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
