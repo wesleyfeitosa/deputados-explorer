ed8f62009b012e03fbbbb7e770ca94da620e0bef
Wesley Feitosa <jwesleydasilva@gmail.com>
2020-05-10 12:22:35 -0300

    page of details

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,26 +1,80 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { useRouteMatch } from 'react-router-dom';
 
-const Details = ({ match }) => {
-  const { id } = match.params;
+import api from '../../services/api';
+
+import { Container } from './styles';
+
+const Details = () => {
+  const { params } = useRouteMatch();
+  const [deputado, setDeputado] = useState({});
 
   useEffect(() => {
-    console.log(id);
-  }, [id]);
+    async function getDeputado() {
+      const { data } = await api.get(`/deputados/${params.id}`);
+
+      setDeputado(data.dados);
+    }
+
+    getDeputado();
+  }, [params.id]);
 
   return (
-    <>
-      <h1>Details</h1>
-    </>
+    <Container>
+      <section>
+        {deputado.ultimoStatus && (
+          <img
+            src={deputado.ultimoStatus.urlFoto}
+            alt={deputado.ultimoStatus.nomeEleitoral}
+          />
+        )}
+        <h1>{deputado.nomeCivil}</h1>
+        <p>CPF: {deputado.cpf}</p>
+        <p>
+          Nascimento: {deputado.dataNascimento}{' '}
+          {deputado.dataFalescimento && (
+            <span>- Falescimento: {deputado.dataFalescimento}</span>
+          )}
+        </p>
+        {deputado.ultimoStatus && <p>E-mail: {deputado.ultimoStatus.email}</p>}
+      </section>
+
+      <section>
+        Naturalidade
+        <p>
+          {deputado.municipioNascimento}/{deputado.ufNascimento}
+        </p>
+      </section>
+
+      <section>
+        Escolaridade
+        <p>{deputado.escolaridade}</p>
+      </section>
+
+      {deputado.ultimoStatus && (
+        <section>
+          Informações políticas
+          <p>Situação: {deputado.ultimoStatus.situacao}</p>
+          <p>Condição Eleitoral: {deputado.ultimoStatus.condicaoEleitoral}</p>
+          <p>Legislatura: {deputado.ultimoStatus.idLegislatura}</p>
+          <p>
+            Partido: {deputado.ultimoStatus.siglaPartido}/
+            {deputado.ultimoStatus.siglaUf}
+          </p>
+        </section>
+      )}
+
+      {deputado.ultimoStatus && deputado.ultimoStatus.gabinete && (
+        <section>
+          Gabinete
+          <p>Sala: {deputado.ultimoStatus.gabinete.sala}</p>
+          <p>Andar: {deputado.ultimoStatus.gabinete.andar}</p>
+          <p>Prédio: {deputado.ultimoStatus.gabinete.predio}</p>
+          <p>Telefone: {deputado.ultimoStatus.gabinete.telefone}</p>
+        </section>
+      )}
+    </Container>
   );
 };
 
 export default Details;
-
-Details.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      id: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
-};
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,3 +1,17 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  > div {
+    color: #34f;
+  }
+
+  section {
+    background-color: #fff;
+    border-radius: 10px;
+    padding: 20px;
+
+    & + section {
+      margin-top: 20px;
+    }
+  }
+`;
